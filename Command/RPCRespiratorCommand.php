970717c6a9a7938f7e2acc1d1770a4de8a9b2d32
Mikolaj <wozniak.mikolaj@outlook.com>
2015-12-07 17:47:02 +0100

    RPC respirator command

--- Command/RPCRespiratorCommand.php
+++ Command/RPCRespiratorCommand.php
@@ -4,6 +4,11 @@ namespace Games\SnakeBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+/**
+ * On production it accured that rpcs disappear after some idle time,
+ * command was created for cron job to ping all rpcs before they disappear
+ * 
+ */
 
 class RPCRespiratorCommand extends ContainerAwareCommand{
     
@@ -13,22 +18,27 @@ class RPCRespiratorCommand extends ContainerAwareCommand{
     protected function execute(InputInterface $input, OutputInterface $output) {
 
         $client = $this->getContainer()->get("thruway.client");
-
-        $data = ["test"=>true];
+        $accesible = true;
+        $rpcs = ["games.snake.newplayer",
+                 "games.snake.move",
+                 "games.snake.startgame",
+                 "games.snake.activity"
+                ];
         try{
-            $output->writeln("probuje wyslac na chat cos");
-                        
-            $client->call("games.snake.newplayer", [["test"=>"test"]])->then(
-                function ($res) use ($output){
-                    $output->writeln( json_encode($res));
-                },  function ($err)use ($output){
-                    $output->writeln( $err);
-                }
-            );
-            
-            
+            $output->writeln("tests rpcs availability");
+            foreach($rpcs as $rpc){
+              $client->call($rpc, [["test"=>"test"]])->then(
+                  function ($res) use ($output){
+                      $output->writeln( json_encode($res));
+                  },  function ($err)use (&$accesible){
+                      $accesible = false;
+                  }
+              );
+            }
+            if(!$accesible) $output->writeln("Przypal");
+       
         } catch (Exception $ex) {
-            $output->writeln("kupa blada");
+            $output->writeln($ex);
         }
 
     }
